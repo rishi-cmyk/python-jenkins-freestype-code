021270b1eedf47188d07459afac02cbbd9dd9314
Rishabh <rishijais19973@gmail.com>
2025-04-05 16:31:27 +0530

    third commit

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from app import hello
 
 class TestApp(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello(), "Hello, Jenkins class 1!")
+        self.assertEqual(hello(), "Hello, Jenkins class!")
 
 if __name__ == "__main__":
     unittest.main()
